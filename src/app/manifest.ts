7ad1c6c9eff12a3f7b5139b824a5500289144c20
jufeza-boop <jufeza@gmail.com>
2026-04-24 17:40:54 +0200

    Update purpose field for PWA icons in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,13 +18,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icons/pwa-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable any",
+        purpose: "maskable",
       },
       {
         src: "/icons/pwa-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable any",
+        purpose: "maskable",
       },
     ],
   };
